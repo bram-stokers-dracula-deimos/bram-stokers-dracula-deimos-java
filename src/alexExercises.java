f7274daf81a5b698cef9ac4a6734d13a613c929a
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2019-11-21 10:43:58 -0600

    fixing

--- src/alexExercises.java
+++ src/alexExercises.java
@@ -1,26 +1,25 @@
 public class alexExercises {
     public static void main(String[] args) {
-        fizzBuzz();
+        System.out.println("FIBONACCI");
+        System.out.println(fibonacci(4));
+        System.out.println(fibonacci(9));
+        System.out.println(fibonacci(12));
+        System.out.println("PALINDROME");
         System.out.println(isPalindrome("racecar") + " true");
         System.out.println(isPalindrome("green") + " false");
     }
-    // standard fizzbuzz function
-    private static void fizzBuzz() {
-        System.out.println("This is Fizzbuzz.");
-        for(int i = 0; i < 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            }
-            else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            }
-            else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            }
-            else {
-                System.out.println(i);
-            }
+    // prints the nth fibonacci number for whatever number is passed
+    private static int fibonacci(int number) {
+        int[] sequence = new int[number + 1];
+        sequence[0] = 0;
+        sequence[1] = 1;
+        System.out.print(sequence[0] +  "," + sequence[1]);
+        for (int i = 2; i < sequence.length; i++){
+            sequence[i] = sequence[i - 1] + sequence[i - 2];
+            System.out.print("," + sequence[i]);
         }
+        System.out.print(" Number: ");
+        return sequence[number];
     }
     // prints reverse of word and checks if palindrome
     private static boolean isPalindrome(String word) {
